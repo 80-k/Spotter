4c43c57b63115fa2f465d9dadf125a1e7165eaae
80-k <jiant-leap@outlook.com>
2025-03-29 18:12:26 +0900

    Initial Commit

---
+++ Spotter/SpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotterApp.swift
+//  Spotter
+//
+//  Created by woo on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotterTests/SpotterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotterTests.swift
+//  SpotterTests
+//
+//  Created by woo on 3/29/25.
+//
+
+import Testing
+@testable import Spotter
+
+struct SpotterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
