610fdde05c382e6b2441af4d8efa145d837feed9
80-k <jiant-leap@outlook.com>
2025-03-30 18:45:10 +0900

    Widget error fixed.

--- SpotterWidgets/SpotterWidgetsBundle.swift
+++ SpotterWidgets/SpotterWidgetsBundle.swift
@@ -5,6 +5,10 @@ import SwiftUI
 @main
 struct SpotterWidgetsBundle: WidgetBundle {
     var body: some Widget {
+        // Regular widgets if you have any
+        SpotterWidgets()
+        
+        // Live Activity for workout tracking
         WorkoutLiveActivity()
     }
 }
